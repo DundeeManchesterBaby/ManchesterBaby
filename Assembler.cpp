bffed1d14cf32b5503926cff474a1472a42c224f
ewanM7 <ewan.mallinson@btinternet.com>
2016-11-20 15:35:14 +0000

    Update Assembler.cpp

--- Assembler.cpp
+++ Assembler.cpp
@@ -14,92 +14,105 @@ bitset<8> Assembler::decimalToBinary(int number){
   int rem;
   int num = number;
   
-  for(int i=0; i<8; i++){
+  for(int i=7; i>=0; i--){
     if(num == 1){
-      *(binary+i) = 1;
+      binary[] = 1;
       return binary;
     }
     else if(num == 0){
       return binary;
     }
     rem = num%2;
-    *(binary+i) = rem;
+    binary[] = rem;
     num = num/2;
   }
 }
 
-char* Assembler::convertLine(string* line){
-  string instructions[7] = {"JMP", "JRP", "LDN", "STO", "SUB", "CMP", "STP"};
+bitset<32> Assembler::convertLine(string* line){
+  string instructions[8] = {"JMP", "JRP", "LDN", "STO", "SUB", "CMP", "STP", "VAR"};
   int pos = 0;
   int counter = 0;
   int iValue = 0;
-  int writePos = 0;
-  char* codeReturn = new char[/*size of max instructions in 1s + 0s*/];
-  while(pos < line.length()-3){
-    for(counter=0; counter<7; counter++){
-      pos = line.find(instructions[counter], pos);
+  int writePos = 15;
+  int varCount = 0;
+  bitset<32> codeReturn = new bitset<32>;
+    for(counter=0; counter<8; counter++){
+      pos = line.find(instructions[counter], pos); //line.find() returns -1 if an instruction is not found
       if(pos != -1){
         iValue = counter;
-        counter = 7;
+        counter = 8;
       }
     }
     if(iValue == 0){
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
+      codeReturn[writePos] = 0;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
     }
     else if(iValue == 1){
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
     }
     else if(iValue == 2){
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
+      codeReturn[writePos]) = 0;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
     }
     else if(iValue == 3){
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
     }
     else if(iValue == 4){
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
+      codeReturn[writePos] = 0;
+      writePos--;
+      codeReturn[writePos] = 0;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
     }
     else if(iValue == 5){
-      *(codeReturn+writePos) = 0;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
+      codeReturn[writePos] = 0;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
     }
     else if(iValue == 6){
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
-      *(codeReturn+writePos) = 1;
-      writePos++;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
+      codeReturn[writePos] = 1;
+      writePos--;
     }
-  }
+    else if(iValue == 7){
+      string tempString;
+      int tempPos = pos;
+      pos = (line.find(":", pos)-1);
+      for(pos>=0; pos--){
+        tempString = tempString + line[pos];
+      }
+      tempString = tempString + ":" + varCount + ":";
+      pos = tempPos + 4;
+      while(line[pos] != " "){
+        tempString = tempString + line[pos];
+      }
+      symbolTable.push_back(tempString);
+    }
   return codeReturn;
 }
 
@@ -108,14 +121,15 @@ void Assembler::read(string fileName){
   ifstream fileStream;
   fileStream.open(fileName);
   
+  
   while(getline(fileStream,currentLine){
-      //loop through the file, storing lines to be implemented
-	}
-    }
+    fileContents.push_back(currentLine);
+  }
+}
   
   void Assembler::write(string fileName){
     ofstream fileStream;
-    // fileStream.open(fileName);
+    fileStream.open(fileName);
     
     // for(//number of iterations){
     //   fileStream << "Output data" << endl;
